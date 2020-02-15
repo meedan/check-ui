b79c8db99f0c25feb699386b1f825f21edc235cd
Piotr F <hello@piotrf.pl>
2020-02-15 12:48:40 -0300

    Clean up.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,25 @@
-import { addParameters } from '@storybook/react';
+import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { addParameters } from '@storybook/react';
 import { configureActions } from '@storybook/addon-actions';
-import '@storybook/addon-console';
 
 addParameters({
+  options: {
+    showRoots: true,
+  },
+  docs: {
+    container: DocsContainer,
+    page: DocsPage,
+  },
+  dependencies: {
+    //display only dependencies/dependents that have a story in storybook
+    //by default this is false
+    withStoriesOnly: false,
+
+    //completely hide a dependency/dependents block if it has no elements
+    //by default this is false
+    hideEmpty: true,
+  },
   viewport: {
     viewports: MINIMAL_VIEWPORTS,
   },
