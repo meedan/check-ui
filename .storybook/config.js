df5f1e895603f0d42a92a8d894757b88423bfcff
Piotr F <hello@piotrf.pl>
2020-02-15 12:16:08 -0300

    Create basic React setup.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import { addParameters } from '@storybook/react';
 import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { configureActions } from '@storybook/addon-actions';
+import '@storybook/addon-console';
 
 addParameters({
   viewport: {
