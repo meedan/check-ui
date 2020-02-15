25b520e583f0ced5a4b5511bebf01da6e189d72f
Piotr F <hello@piotrf.pl>
2020-02-15 12:48:11 -0300

    Add Props of to the docs.

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class Timeline extends React.Component {
   render() {
     return <div>Timeline</div>;
   }
 }
+
+Timeline.propTypes = {
+  data: PropTypes.string,
+};
+
+Timeline.defaultProps = {
+  data: '',
+};
