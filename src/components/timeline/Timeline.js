f973e9b14e7db13c6f5455f31e9b61240e413f5a
Piotr F <hello@piotrf.pl>
2020-02-16 19:48:21 -0300

    Document more Timeline proptypes.

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -6,6 +6,8 @@ import Table from '@material-ui/core/Table';
 import Playhead from './playhead/Playhead';
 import Entities from './entities/Entities';
 
+const SIDE = 224;
+
 const useStyles = makeStyles({
   timelineRoot: {
     position: 'relative',
@@ -14,18 +16,26 @@ const useStyles = makeStyles({
 
 const Timeline = props => {
   const classes = useStyles();
-  const { currentTime, duration, onChange } = props;
+  const { currentTime, duration, onChange, playing } = props;
+
+  // console.log({ props });
+
   return (
     <div className={classes.timelineRoot}>
       <Playhead
         max={duration}
         onChange={onChange}
-        style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: `${SIDE}px`,
+          right: 0,
+          bottom: 0,
+        }}
         value={currentTime}
       />
-      <Table padding="checkbox">
+      <Table>
         <Entities
-          // playing={playing}
           // skip={skip}
           // timelineOffset={props.x1}
           // transport={transport}
@@ -38,11 +48,11 @@ const Timeline = props => {
           onAfterChange={args => console.log('onAfterChange', args)}
           onBeforeChange={args => console.log('onBeforeChange', args)}
           onChange={args => console.log('onChange', args)}
+          playing={playing}
           suggestions={props.data.project.projectclips}
           title="Clips"
         />
         <Entities
-          // playing={playing}
           // skip={skip}
           // timelineOffset={props.x1}
           // transport={transport}
@@ -56,11 +66,11 @@ const Timeline = props => {
           onAfterChange={args => console.log('onAfterChange', args)}
           onBeforeChange={args => console.log('onBeforeChange', args)}
           onChange={args => console.log('onChange', args)}
+          playing={playing}
           suggestions={props.data.project.projecttags}
           title="Tags"
         />
         <Entities
-          // playing={playing}
           // skip={skip}
           // timelineOffset={props.x1}
           // transport={transport}
@@ -74,6 +84,7 @@ const Timeline = props => {
           onAfterChange={args => console.log('onAfterChange', args)}
           onBeforeChange={args => console.log('onBeforeChange', args)}
           onChange={args => console.log('onChange', args)}
+          playing={playing}
           suggestions={props.data.project.projectplaces}
           title="Places"
         />
@@ -83,14 +94,37 @@ const Timeline = props => {
 };
 
 Timeline.propTypes = {
+  data: PropTypes.shape({
+    commentThreads: PropTypes.array,
+    project: PropTypes.shape({
+      projectclips: PropTypes.array,
+      projectplaces: PropTypes.array,
+      projecttags: PropTypes.array,
+    }),
+    videoClips: PropTypes.array,
+    videoPlaces: PropTypes.array,
+    videoTags: PropTypes.array,
+  }),
   currentTime: PropTypes.number,
-  duration: PropTypes.number,
+  duration: PropTypes.number.isRequired,
   onChange: PropTypes.func.isRequired,
+  playing: PropTypes.bool,
 };
 
 Timeline.defaultProps = {
+  data: {
+    commentThreads: [],
+    project: {
+      projectclips: [],
+      projectplaces: [],
+      projecttags: [],
+    },
+    videoClips: [],
+    videoPlaces: [],
+    videoTags: [],
+  },
   currentTime: 0,
-  duration: null,
+  playing: false,
 };
 
 export default Timeline;
